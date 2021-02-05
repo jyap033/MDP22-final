b9457d4cdd3e06220e5b6b553ae946d14df1fb24
zhengtao99 <zhengtaolim@gmail.com>
2021-02-05 09:18:00 +0800

    Server Initial Commit

---
+++ Server/AlgorithmManager.py
@@ -0,0 +1,15 @@
+# Initializing a queue
+movements = []
+    
+class Algorithm:
+
+    def compute(self):       
+        # Adding elements to the queue
+        movements.append("L10")
+        movements.append("S5")
+        movements.append("R6")
+
+    def getmovements(self):
+        return movements
+        
+        
