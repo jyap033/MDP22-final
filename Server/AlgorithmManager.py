9ae1ce62c0c1b33a24a3beee4d04702402f44a7a
YangXingHao96 <60702299+YangXingHao96@users.noreply.github.com>
2021-02-23 13:35:07 +0800

    Add files via upload

--- Server/AlgorithmManager.py
+++ Server/AlgorithmManager.py
@@ -1,13 +1,31 @@
-# Initializing a queue
-movements = []
-    
-class Algorithm:
+from Map import Preprocess_Map
+from Shortest_path import A_star
 
-    def compute(self):       
-        # Adding elements to the queue
-        movements=['U', 'R', 'R', 'R', 'R', 'R', 'U', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'U', 'U', 'U', 'U', 'L', 'L', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'U', 'R', 'R', 'U', 'U']
+class AlgorithmManager:
+    def compute(self,hex,mid_point):
+            
+        sol=A_star()
+        p1,p2,dir=sol.get_path(hex,mid_point)
+        path=p1+p2[1:]
 
-    def getmovements(self):
-        return movements
-        
-        
+        for i in range(len(dir)):
+            if dir[i]=="N":
+                dir[i]="S"
+            elif dir[i]=="S":       
+                dir[i]="N"
+            else:
+                continue
+        pre=Preprocess_Map()
+        mat=pre.generate_path(hex,path)
+        for i in mat:
+            print(i)
+        movements=sol.process_directions(dir)
+        print(movements)
+        new_mov=sol.process_mov(movements)
+        print(new_mov)
+        return new_mov
+hex=hex="000000000000010042038400000000000000030C000000000000021F8400080000000000040"
+waypoint=[3,2]
+sol=AlgorithmManager()
+movs=sol.compute(hex,waypoint)
+print(movs)
